5499be5e986ce90033649a9838c93a74fbd398b1
Mohamed Aboellil <mohamed.aboellil0@gmail.com>
2025-04-23 09:27:35 +0200

    min stack

---
+++ leetCode/155. Min Stack.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetCode
+{
+    public class MinStack
+    {
+        private Stack<int> stack = new Stack<int>();
+        public MinStack()
+        {
+
+        }
+
+        public void Push(int val)
+        {
+            stack.Push(val);
+        }
+
+        public void Pop()
+        {
+            stack.Pop();
+        }
+
+        public int Top()
+        {
+            return stack.Pop();
+        }
+
+        public int GetMin()
+        {
+           return stack.Min();
+        }
+    }
+}
--- leetCode/Program.cs
+++ leetCode/Program.cs
@@ -6,6 +6,18 @@ using leetCode;
 
 //Console.WriteLine(asd.IndexOf(prefix));
 
-_20 _20 = new _20();
+//_20 _20 = new _20();
 
-Console.WriteLine(_20.IsValid("([}}])"));
+//Console.WriteLine(_20.IsValid("([}}])"));
+
+
+
+//MinStack minStack = new MinStack();
+//minStack.Push(-2);
+//minStack.Push(0);
+//minStack.Push(-3);
+//minStack.GetMin(); // return -3
+//minStack.Pop();
+//minStack.Top();    // return 0
+//minStack.GetMin(); // return -2
+
